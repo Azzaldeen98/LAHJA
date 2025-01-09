5f84c1feaf286cab83183ef472d601b05a449c78
Azzaldeen98 <azzaldeen771211417@gmail.com>
2025-01-09 22:08:52 +0300

    AZM commit 70

---
+++ LAHJA/Data/UI/Components/StudioLahjaAiVM/FilterItemData.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace LAHJA.Data.UI.Components.StudioLahjaAiVM
+{
+    public class FilterItemData
+    {
+        public int Id { get; set; } = 0;
+        public string Identifier { get; set; } = string.Empty;
+        public Dictionary<string, string> Text { get; set; } = new();
+        public string Icon { get; set; } = string.Empty;
+        public Type? Component { get; set; }
+        public string TypeModel { get; set; } = string.Empty;
+    }
+
+    public class StudioFilterDefinition<T>
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Icon { get; set; } = string.Empty;
+        public List<T> Options { get; set; } = new();
+        public EventCallback<T> OnSelectionChanged { get; set; }
+    }
+
+}
